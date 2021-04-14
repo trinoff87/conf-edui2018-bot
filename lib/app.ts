5d1ecb94dedda126044a1238d1c762bdf1e1db41
Trino Fajardo <trinoff87@gmail.com>
2021-04-13 20:04:46 -0500

    Refactor to use onTurn

--- lib/app.ts
+++ lib/app.ts
@@ -1,8 +1,8 @@
-import {BotFrameworkAdapter, ConversationState, MemoryStorage, UserState } from "botbuilder";
+import {BotFrameworkAdapter} from "botbuilder";
 import * as restify from "restify";
-import { ConfState} from "./types";
+import {EchoBot} from "./bot"
 
-let server = restify.createServer();
+const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
     console.log(`${server.name} listening on ${server.url}`);
 });
@@ -11,18 +11,10 @@ const adapter = new BotFrameworkAdapter({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-const memoryStorage = new MemoryStorage();
-const conversationSate = new ConversationState(memoryStorage);
-let userState: UserState;
-userState = new UserState(memoryStorage)
 
+const echo:EchoBot = new EchoBot();
 server.post("/api/messages", (req, res) => {
     adapter.processActivity(req, res, async (context) => {
-        if (context.activity.type === "message") {
-            const state = conversationSate.get(context);
-            await context.sendActivity(`You said ${context.activity.text}`);
-        } else {
-            await context.sendActivity(`${context.activity.type} event detected`)
-        }
+        await echo.onTurn(context);
     });
 })
